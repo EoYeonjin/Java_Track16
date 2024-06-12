fa8db5887d4ea539c80202f7456089f2a0840b3a
EoYeonjin <wks1211@gmail.com>
2024-06-12 16:46:52 +0900

    Add files via upload

--- homepage_one/src/dao/MemberDao.java
+++ homepage_one/src/dao/MemberDao.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import common.DBConnection;
+import dto.MemberDto;
 
 public class MemberDao {
 	Connection con;
@@ -36,5 +37,34 @@ public class MemberDao {
 		}
 		
 		return count;
+	}
+	
+	//register
+	public int memberSave(MemberDto dto) {
+		int result = 0;
+		String query = "insert into jsl_어연진_member\r\n" + 
+				"(id, name, password, job, \r\n" + 
+				"tell_1, tell_2, tell_3, \r\n" + 
+				"mobile_1, mobile_2, mobile_3, \r\n" + 
+				"email_1, email_2, reg_date)\r\n" + 
+				"values\r\n" + 
+				"('"+dto.getId()+"',	'"+dto.getName()+"', '"+dto.getPassword()+"', '"+dto.getJob()+"',\r\n" + 
+				"'"+dto.getTell_1()+"', '"+dto.getTell_2()+"', '"+dto.getTell_3()+"',\r\n" + 
+				"'"+dto.getMobile_1()+"', '"+dto.getMobile_2()+"', '"+dto.getMobile_3()+"',\r\n" + 
+				"'"+dto.getEmail_1()+"', '"+dto.getEmail_2()+"', to_date('"+dto.getReg_date()+"','yyyy-MM-dd hh24:mi:ss')\r\n" + 
+				")";
+		
+		try {
+			con = DBConnection.getConnection();
+			ps = con.prepareStatement(query);
+			result = ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("memberSave() method error\n"+query);
+			e.printStackTrace();
+		} finally {
+			DBConnection.closeDB(con, ps, rs);
+		}
+		
+		return result;
 	}
 }
