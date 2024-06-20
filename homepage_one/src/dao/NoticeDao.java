af7c13155c08b91edee84b264411b48adc16eb98
EoYeonjin <wks1211@gmail.com>
2024-06-20 16:44:28 +0900

    Add files via upload

--- homepage_one/src/dao/NoticeDao.java
+++ homepage_one/src/dao/NoticeDao.java
@@ -62,23 +62,20 @@ public class NoticeDao {
 		return result;
 	}
 	
-	//목록
-	public ArrayList<NoticeDto> getNoticeList(String select, String search){
+	//목록조회
+	public ArrayList<NoticeDto> getNoticeList(String select, String search, int start, int end){
 		ArrayList<NoticeDto> dtos = new ArrayList<>();
-		String query = "select n.no, n.title, m.name as reg_name, \r\n" + 
-				"to_char(n.reg_date, 'yyyy-MM-dd') as reg_date, n.hit\r\n" + 
-				"from jsl_어연진_member m, jsl_어연진_notice n\r\n" + 
-				"where m.id = n.reg_id\r\n" + 
-				"and n."+select+" like '%"+search+"%'\r\n" + 
-				"order by to_number(no) desc";
-		
-		if(select.equals("")) {
-			query = "select n.no, n.title, m.name as reg_name, \r\n" + 
-					"to_char(n.reg_date, 'yyyy-MM-dd') as reg_date, n.hit\r\n" + 
-					"from jsl_어연진_member m, jsl_어연진_notice n\r\n" + 
-					"where m.id = n.reg_id\r\n" + 
-					"order by to_number(no) desc";
-		}
+		String query = "select * from(\r\n" + 
+				"    select rownum as rnum, tbl.*\r\n" + 
+				"    from(\r\n" + 
+				"        select n.no, n.title, m.name as reg_name, \r\n" + 
+				"        to_char(n.reg_date, 'yyyy-MM-dd') as reg_date, n.hit\r\n" + 
+				"        from jsl_어연진_member m, jsl_어연진_notice n\r\n" + 
+				"        where m.id = n.reg_id\r\n" + 
+				"        and "+select+" like '%"+search+"%'\r\n" + 
+				"        order by to_number(no) desc\r\n" + 
+				") tbl)\r\n" + 
+				"where rnum>= "+start+" and rnum<="+end;
 		
 		try {
 			con = DBConnection.getConnection();
@@ -102,5 +99,64 @@ public class NoticeDao {
 		}
 		
 		return dtos;
+	}
+	
+	//목록 건수
+	public int getTotalCount(String select, String search) {
+		int count = 0;
+		String query = "select count(*) as count\r\n" + 
+				"from jsl_어연진_notice\r\n" + 
+				"where "+select+" like '%"+search+"%'";
+		
+		try {
+			con = DBConnection.getConnection();
+			ps = con.prepareStatement(query);
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				count = rs.getInt("count");
+			}
+		} catch (SQLException e) {
+			System.out.println("getTotalCount() method error\n"+query);
+			e.printStackTrace();
+		} finally {
+			DBConnection.closeDB(con, ps, rs);
+		}
+		
+		return count;
+	}
+	
+	//상세조회
+	public NoticeDto getNoticeView(String no) {
+		NoticeDto dto = null;
+		String query = "select n.title, n.reg_id, m.name as reg_name, n.content, n.hit,\r\n" + 
+				"to_char(n.reg_date, 'yyyy-MM-dd') as reg_date\r\n" + 
+				"from jsl_어연진_member m, jsl_어연진_notice n\r\n" + 
+				"where m.id = n.reg_id \r\n" + 
+				"and no = '"+no+"'";
+		
+		try {
+			con = DBConnection.getConnection();
+			ps = con.prepareStatement(query);
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				String title = rs.getString("title");
+				String content = rs.getString("content");
+				String reg_id = rs.getString("reg_id");
+				String reg_name = rs.getString("reg_name");
+				String reg_date = rs.getString("reg_date");
+				int hit = rs.getInt("hit");
+				
+				dto = new NoticeDto(no, title, content, reg_id, reg_name, reg_date, hit);
+			}
+		} catch (SQLException e) {
+			System.out.println("getNoticeView() method error\n"+query);
+			e.printStackTrace();
+		} finally {
+			DBConnection.closeDB(con, ps, rs);
+		}
+		
+		return dto;
 	}
 }
