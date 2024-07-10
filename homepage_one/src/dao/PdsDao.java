352ec1852415782667dff614cc713c5003d16ad5
EoYeonjin <wks1211@gmail.com>
2024-07-10 17:04:59 +0900

    Add files via upload

--- homepage_one/src/dao/PdsDao.java
+++ homepage_one/src/dao/PdsDao.java
@@ -124,4 +124,127 @@ public class PdsDao {
 		
 		return totalCount;
 	}
+	
+	//조회수 증가
+	public int updateHit(String no) {
+		int result = 0;
+		String query = "update JSL_어연진_PDS\r\n" + 
+				"set hit = hit + 1\r\n" + 
+				"where no = '"+no+"'";
+		
+		try {
+			con = DBConnection.getConnection();
+			ps = con.prepareStatement(query);
+			result = ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("updateHit() method error\n"+query);
+			e.printStackTrace();
+		} finally {
+			DBConnection.closeDB(con, ps, rs);
+		}
+		
+		return result;
+	}
+	
+	//상세조회
+	public PdsDto getPdsView(String no) {
+		PdsDto dto = null;
+		String query = "select p.title, p.content, m1.name as reg_name, p.attach,\r\n" + 
+				"to_char(p.reg_date, 'yyyy-MM-dd hh24:mi:ss') as reg_date, p.hit,\r\n" + 
+				"to_char(p.update_date, 'yyyy-MM-dd hh24:mi:ss') as update_date, m2.name as update_name\r\n" +
+				"from JSL_어연진_PDS p, jsl_어연진_member m1, jsl_어연진_member m2\r\n" + 
+				"where p.reg_id = m1.id\r\n" + 
+				"and p.update_id = m2.id(+)\r\n" +
+				"and p.no = '"+no+"'";
+		
+		try {
+			con = DBConnection.getConnection();
+			ps = con.prepareStatement(query);
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				String title = rs.getString("title");
+				String content = rs.getString("content");
+				String reg_name = rs.getString("reg_name");
+				String attach = rs.getString("attach");
+				if(attach == null) attach = "첨부파일 없음";
+				String reg_date = rs.getString("reg_date");
+				String update_name = rs.getString("update_name");
+				String update_date = rs.getString("update_date");
+				int hit = rs.getInt("hit");
+				
+				
+				dto = new PdsDto(title, content, attach, reg_name, reg_date, update_name, update_date, hit);
+			}
+		} catch (SQLException e) {
+			System.out.println("getPdsList() method error\n"+query);
+			e.printStackTrace();
+		} finally {
+			DBConnection.closeDB(con, ps, rs);
+		}
+		
+		return dto;
+	}
+	
+	//이전글
+	public PdsDto getPrePds(String no) {
+		PdsDto preDto = null;
+		String query = "select p.no, p.title\r\n" + 
+				"from \r\n" + 
+				"    (select max(to_number(no)) as no\r\n" + 
+				"    from JSL_어연진_PDS\r\n" + 
+				"    where no < "+no+") a, jsl_어연진_pds p\r\n" + 
+				"where a.no = p.no";
+		
+		try {
+			con = DBConnection.getConnection();
+			ps = con.prepareStatement(query);
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				no = rs.getString("no");
+				String title = rs.getString("title");
+				
+				preDto = new PdsDto(no, title);
+			}
+		} catch (SQLException e) {
+			System.out.println("getPrePds() method error\n"+query);
+			e.printStackTrace();
+		} finally {
+			DBConnection.closeDB(con, ps, rs);
+		}
+		
+		return preDto;
+	}
+	
+	//다음글
+	public PdsDto getNextPds(String no) {
+		PdsDto nexDto = null;
+		String query = "select p.no, p.title\r\n" + 
+				"from \r\n" + 
+				"    (select min(to_number(no)) as no\r\n" + 
+				"    from JSL_어연진_PDS\r\n" + 
+				"    where no > "+no+") a, jsl_어연진_pds p\r\n" + 
+				"where a.no = p.no";
+		
+		try {
+			con = DBConnection.getConnection();
+			ps = con.prepareStatement(query);
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				no = rs.getString("no");
+				String title = rs.getString("title");
+				
+				nexDto = new PdsDto(no, title);
+			}
+		} catch (SQLException e) {
+			System.out.println("getNexPds() method error\n"+query);
+			e.printStackTrace();
+		} finally {
+			DBConnection.closeDB(con, ps, rs);
+		}
+		
+		return nexDto;
+	}
 }
--- homepage_one/src/dto/PdsDto.java
+++ homepage_one/src/dto/PdsDto.java
@@ -3,7 +3,28 @@ package dto;
 public class PdsDto {
 	private String no, title, content, attach, reg_id, reg_name, reg_date, update_id, update_name, update_date;
 	private int hit;
-	
+
+	//이전글, 다음글
+	public PdsDto(String no, String title) {
+		super();
+		this.no = no;
+		this.title = title;
+	}
+
+	//상세조회
+	public PdsDto(String title, String content, String attach, String reg_name, String reg_date, String update_name,
+			String update_date, int hit) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.attach = attach;
+		this.reg_name = reg_name;
+		this.reg_date = reg_date;
+		this.update_name = update_name;
+		this.update_date = update_date;
+		this.hit = hit;
+	}
+
 	//조회
 	public PdsDto(String no, String title, String attach, String reg_name, String reg_date, int hit) {
 		super();
